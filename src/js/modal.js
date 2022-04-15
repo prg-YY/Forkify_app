f06bbc7e58ea8e1a4254f735a48f4a33c979c616
prg0615 <parguzatt@gmail.com>
2022-04-15 22:36:09 +0200

    ad

--- src/js/modal.js
+++ src/js/modal.js
@@ -4,6 +4,10 @@ import { API_URL } from './config.js';
 
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
 };
 
 export const loadRecipe = async id => {
@@ -21,8 +25,30 @@ export const loadRecipe = async id => {
       cookingTime: recipe.cooking_time,
       title: recipe.title,
     };
-    console.log(state.recipe);
+    // console.log(state.recipe);
   } catch (err) {
     console.error(`${err}💣💣💣💣 `);
+    throw err;
+  }
+};
+
+export const loadSearchResults = async query => {
+  try {
+    state.search.results = query;
+
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+  } catch (err) {
+    console.error(`${err}💣💣💣💣 `);
+    throw err;
   }
 };
