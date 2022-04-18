f092aa113c65e732b4aa52c54be07643de04738b
prg0615 <parguzatt@gmail.com>
2022-04-18 21:23:16 +0200

    completed

--- src/js/views/view.js
+++ src/js/views/view.js
@@ -3,14 +3,55 @@ import icons from 'url:../../img/icons.svg'; //Parcel 2
 export default class View {
   _data;
 
-  render(data) {
+  /**
+   * Render the received object tot the DOM
+   * @param {Object|Object[]} data The data to be rendered (e.g.recipe)
+   * @param {boolean}[ render=true] if false ,create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render=false
+   * @this {View}  View instance
+   * @author Yusufu yahefu
+   * @todo Finish implementation
+   */
+  render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
 
     this._data = data;
     const markup = this._generateMarkup();
+
+    if (!render) return markup;
+
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  update(data) {
+    this._data = data;
+    const newMarkup = this._generateMarkup();
+
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+    const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+      // console.log(curEl, newEl.isEqualNode(curEl));
+
+      // Updates changed TEXT
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        // console.log('💥', newEl.firstChild.nodeValue.trim());
+        curEl.textContent = newEl.textContent;
+      }
+
+      // Updates changed ATTRIBUES
+      if (!newEl.isEqualNode(curEl))
+        Array.from(newEl.attributes).forEach(attr =>
+          curEl.setAttribute(attr.name, attr.value)
+        );
+    });
   }
 
   _clear() {
